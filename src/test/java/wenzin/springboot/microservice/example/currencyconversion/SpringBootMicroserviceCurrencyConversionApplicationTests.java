a86bd23047a34eb8c222581c7def8533f9bc4b09
Helena Wenzin <helma254@hotmail.com>
2020-10-14 18:10:26 +0200

    First commit

---
+++ src/test/java/wenzin/springboot/microservice/example/currencyconversion/SpringBootMicroserviceCurrencyConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package wenzin.springboot.microservice.example.currencyconversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceCurrencyConversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
